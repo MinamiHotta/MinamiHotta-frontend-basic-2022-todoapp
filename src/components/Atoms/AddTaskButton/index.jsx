e9b76f5c11c55b625f8f55bcd397bb45a6606e17
MinamiHotta <m.hotta@4nonome.com>
2022-08-03 00:16:54 +0900

    hover時に背景が出ない

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 import plus from "../../../assets/svg/plus.svg";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
 
-const AddTaskButton = () => {
+const AddTaskButton = ({checked}) => {
   return (
-    <StyledButton>
+    <StyledAddButton onClick={checked}>
+      <HoverBack/>
       <Img src={plus} />
-      <StyledText>タスクを追加</StyledText>
-    </StyledButton>
+      <StyledAddText>タスクを追加</StyledAddText>
+    </StyledAddButton>
   );
 };
 export default AddTaskButton;
 
-//TODO:背景はhover時に出現するように
-const StyledButton = styled.button`
+const HoverBack = styled.div`
+  background-color: ${COLOR.GREEN};
+  opacity: 0;
+`;
+const StyledAddButton = styled.button`
+  display:flex;
   width: 126px;
   height: 24px;
-  padding: 2px;
+  padding: 2px 6px;
   border-radius: 12px;
-  background-color: ${COLOR.GREEN};
-  opacity: 0.2;
+  background-color:transparent;
+  border:none;
+  &:hover > ${HoverBack}{
+    opacity: 0.2;
+    cursor:pointer;
+  }
 `;
 
 const Img = styled.img`
@@ -27,11 +38,14 @@ const Img = styled.img`
   width: 20px;
   height: 20px;
   margin-right: 10px;
+  z-index:10;
 `;
 
-const StyledText = styled.div`
+const StyledAddText = styled.div`
   color: ${COLOR.GREEN};
-  ${SIZE.S};
+  ${TEXT.S}
+  z-index:10;
 `;
+
 
 //TODO:新しくstyleを付加するには
