1ed55b46b880a68637d5a88f16a03f440ef710d7
MinamiHotta <m.hotta@4nonome.com>
2022-09-23 00:16:11 +0900

    AlertManagerをuseEffect使って書き換え

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 import { useAlertHandlerContext } from "../../../contexts/alert_handler";
+import Alert from "../../Atoms/Alert";
 
 const AlertManager = () => {
   const AlertHandlerContext = useAlertHandlerContext();
+  useEffect(() => {
+    if (!AlertHandlerContext.visible) {
+      return;
+    }
+    setTimeout(AlertHandlerContext.closeAlert, 5000);
+  }, [AlertHandlerContext]);
 
-  if (AlertHandlerContext.visible === true) {
-    AlertHandlerContext.setAlert("タスクの名前が設定されていません。");
-    startAlert();
-  }
-
-  startAlert = () => {
-    document.getElementsById("temporaryAlert").style.display = "block";
-    setTimeout(closeAlert, 5000);
-  };
-  closeAlert = () => {
-    document.getElementsById("temporaryAlert").style.display = "none";
-  };
+  return (
+    <Alert
+      errorText={AlertHandlerContext.errorText}
+      visible={AlertHandlerContext.visible}
+    />
+  );
 };
 export default AlertManager;
