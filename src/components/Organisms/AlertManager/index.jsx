8d639bd1f03ac71678fbe5330d5318d77f24eb01
MinamiHotta <m.hotta@4nonome.com>
2022-09-23 11:12:36 +0900

    Alertを中央に配置

--- src/components/Organisms/AlertManager/index.jsx
+++ src/components/Organisms/AlertManager/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useAlertHandlerContext } from "../../../contexts/alert_handler";
 import Alert from "../../Atoms/Alert";
+import styled from "styled-components";
 
 const AlertManager = () => {
   const AlertHandlerContext = useAlertHandlerContext();
@@ -13,10 +14,16 @@ const AlertManager = () => {
   }, [AlertHandlerContext]);
 
   return (
-    <Alert
-      errorText={AlertHandlerContext.errorText}
-      visible={AlertHandlerContext.visible}
-    />
+    <Container>
+      <Alert
+        errorText={AlertHandlerContext.errorText}
+        visible={AlertHandlerContext.visible}
+      />
+    </Container>
   );
 };
 export default AlertManager;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
