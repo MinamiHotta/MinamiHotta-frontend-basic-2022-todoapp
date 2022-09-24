1775b91c5d3b89b1eb8168e0344fac995b47ae23
MinamiHotta <m.hotta@4nonome.com>
2022-09-24 12:32:51 +0900

    コメントアウトを完全消去

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-// import { AlertHandlerProvider } from "../../../contexts/alert_handler";
 import Title from "../../Atoms/Title";
-// import AlertManager from "../../Organisms/AlertManager";
 import TodoCard from "../../Organisms/TodoCard";
 import BREAKPOINT from "../../../variables/breakpoint";
 
 const MainPage = () => {
   return (
-    //<AlertHandlerProvider>
-    //<AlertManager />はContainerの中にあった
-
     <Container>
       <Title />
       <TodoCard />
     </Container>
-    //</AlertHandlerProvider>
   );
 };
 export default MainPage;
